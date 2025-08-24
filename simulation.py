a96485180c2cfd4ae2fbb448b2d919365aa105f9
Detroix23 <detroix23@gmail.com>
2025-08-24 12:09:14 +0300

    Collisions V1.

--- simulation.py
+++ simulation.py
@@ -9,7 +9,6 @@ from modules.maths_local import *
 import ui
 import modules.settings
 from typing_extensions import Self
-import copy
 
 # GUI
 class Board:
@@ -315,9 +314,7 @@ class Elem:
     def move(self) -> None:
         """
         Move the elem, according to force vector at a scale (mass) and checking collision
-        """
-        # Apply optional displacement
-        self.position.add(self.displacement)
+        """ 
         # Apply force
         self.velocity = Vector2D(
             x = self.velocity.x + self.force_vector.x / (self.mass * self.BOARD.mass_softener),
@@ -328,6 +325,8 @@ class Elem:
             x = self.position.x + self.velocity.x,
             y = self.position.y + self.velocity.y
         )
+         # Apply optional displacement
+        self.position.add(self.displacement)
         # Check edges
         """ Disabled for now """
         # Reset values
@@ -491,20 +490,8 @@ def collision(a: Elem, b: Elem, behaviour: modules.settings.CollisionsBehaviour)
     To avoid the effect to cancel itself, each Elem has a list of already collided elements.
     Return True if collision actually happened, False otherwise
     """
-    # Lightest element
-    lightest: Elem
-    heaviest: Elem
-    if a.mass > b.mass:
-        lightest = b
-        heaviest = a
-    else:
-        lightest = a
-        heaviest = b
-    distance_min = heaviest.size + lightest.size
     collision_state: bool = False
-    
-    if a not in b.collisions or b not in a.collisions:
-        
+    if a not in b.collisions and b not in a.collisions:   
         # Detroix23 collision simplification 4, using a medium vector n, affected by mass and direction, that reflect the velocity vectors.
         n: Vector2D = a.velocity * a.mass + b.velocity * b.mass
         n.normalize()
@@ -515,16 +502,23 @@ def collision(a: Elem, b: Elem, behaviour: modules.settings.CollisionsBehaviour)
         a.collisions.append(b)
         b.collisions.append(a)
         collision_state = True
+        # Check where the elems are going to land.
+        distance_min = a.size / 2 + b.size / 2
+        future_position_a: Vector2D = a.position + a.velocity
+        future_position_b: Vector2D = b.position + b.velocity
+        future_distance: float = math.sqrt((future_position_a.x - future_position_b.x) ** 2 + (future_position_a.y - future_position_b.y) ** 2)
         # Try to unclip
-        if lightest.velocity.magnitude < distance_min:
+        if future_distance < distance_min:
             # Collision unclip.
-            v: Vector2D = Vector2D(lightest.position.x - heaviest.position.x, lightest.position.y - heaviest.position.y)
+            v: Vector2D = Vector2D(future_position_b.x - future_position_a.x, future_position_b.y - future_position_a.y)
             d: float = v.magnitude
             v.normalize()
-            lightest.displacement = v * (1 - d + lightest.size)
+            displacement: Vector2D = v * (a.size / 2 - d + b.size / 2)
+            n_a: float = - b.mass / (a.mass + b.mass)
+            n_b: float = a.mass / (a.mass + b.mass)
 
-
-
-
+            a.displacement = Vector2D(displacement.x, displacement.y) * n_a
+            b.displacement = Vector2D(displacement.x, displacement.y) * n_b
+            print(f"! C - Fu: {a.displacement=} {n_a}, {b.displacement=} {n_b}; ")
     return collision_state
     
